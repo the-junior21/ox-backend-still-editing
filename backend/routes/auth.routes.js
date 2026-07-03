8d1bb1582f5abc7d40caf865b3930a3c5d0b07ab
the-junior21 <hamdidajr@gmail.com>
2026-07-03 11:04:44 +0100

    add the login logic route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,21 +1,46 @@
-import express from "express";
 import bcrypt from "bcryptjs";
+import express from "express";
 import User from "../models/User.js";
 
 const router = express.Router();
-
+router.post("/login", async (req, res) => {
+  console.log("body:", req.body);
+  const { email, password } = req.body;
+  if (!email?.trim() || !password?.trim()) {
+    return res.status(400).json({ message: "Missing fields" });
+  }
+  try {
+    const user = await User.findOne({
+      email: email.trim().toLowerCase(),
+    });
+    if (!user) {
+      return res.status(400).json({ message: "Email not found " });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    return res.status(200).json({
+      message: "Login successful",
+      userId: user._id,
+      role: user.role,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error during login" });
+  }
+});
 router.post("/signup", async (req, res) => {
-    console.log("BODY:", req.body);
+  console.log("BODY:", req.body);
 
-  const { name,email,number,password} = req.body;
+  const { name, email, number, password } = req.body;
 
-  if (!name?.trim() || !email?.trim() || !number?.trim()|| !password?.trim()) {
-  return res.status(400).json({ message: "Missing fields" });
-}
+  if (!name?.trim() || !email?.trim() || !number?.trim() || !password?.trim()) {
+    return res.status(400).json({ message: "Missing fields" });
+  }
   try {
     const existingEmail = await User.findOne({
-      email:email.trim().toLowerCase(),
-    })
+      email: email.trim().toLowerCase(),
+    });
     if (existingEmail) {
       return res.status(400).json({
         message: "Email already exists",
@@ -31,26 +56,20 @@ router.post("/signup", async (req, res) => {
     }
     const hashed = await bcrypt.hash(password, 10);
 
-     const user = await User.create({
-          name:name.trim(),
-          email:email.trim(),
-          number:number.trim(),
-          password:hashed,
-          role:null,
-        })
-     
-  //    email,
-    //  password: hashed;
-      //role : role || null,
-   // ;
+    const user = await User.create({
+      name: name.trim(),
+      email: email.trim(),
+      number: number.trim(),
+      password: hashed,
+      role: null,
+    });
 
     res.status(201).json({
       message: "User created",
-      userId :user._id.toString(),
-     });
-
+      userId: user._id.toString(),
+    });
   } catch (err) {
-    console.error(err)
+    console.error(err);
     if (err.code === 11000)
       return res.status(400).json({ message: "Numebr exists" });
 
