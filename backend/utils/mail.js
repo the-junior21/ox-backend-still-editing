bb1cef4d18ec5afeb4ae07b99cc3c3bd303810da
the-junior21 <hamdidajr@gmail.com>
2026-07-04 17:28:02 +0100

    add the verification part 1.2

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -2,12 +2,13 @@ import nodemailer from "nodemailer"
 
 
 
+
 const transporter = nodemailer.createTransport({
-    host:process.MAILTRAP_HOST,
+    host:process.env.MAILTRAP_HOST,
     port:Number(process.env.PORT),
     auth:{
         user:process.env.MAILTRAP_USER,
-        pass:process.MAILTRAP_PASS,
+        pass:process.env.MAILTRAP_PASS,
     },
 })
 export default transporter
