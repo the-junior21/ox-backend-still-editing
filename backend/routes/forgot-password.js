1540f1270d345f96750a63e0d292a6db60650b30
the-junior21 <hamdidajr@gmail.com>
2026-07-09 00:00:17 +0100

    add rest password route 1.1

--- backend/routes/forgot-password.js
+++ backend/routes/forgot-password.js
@@ -1,7 +1,7 @@
+import bcrypt from "bcryptjs";
 import express from "express";
 import User from "../models/User.js";
 import resend from "../utils/mail.js"; // adjust to your setup
-
 const router = express.Router();
 
 router.post("/forgot-password", async (req, res) => {
