35b1bc85553e515eb847e29436055e2fdc19eb9e
the-junior21 <hamdidajr@gmail.com>
2026-07-23 15:36:20 +0100

    add admin route 1.1

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,7 +3,7 @@ import Drievr from "../models/Driver.js"
 export const getPendingDrivers = async (req,res)=>{
     try{
         const drivers = await Driver.find({
-            status:"Pending",
+            status:"pending",
         })
         res.status(200).json({
             success:true,
