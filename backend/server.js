bc5f71ed2d041f79990d1c819bceb2a1119dd702
the-junior21 <hamdidajr@gmail.com>
2026-07-25 18:03:34 +0100

    update the status of the driver 1.5

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS,PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
