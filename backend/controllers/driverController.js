9b2b46a76a3534d2d9afe0019893e91dc53194bf
the-junior21 <hamdidajr@gmail.com>
2026-07-27 07:02:06 +0100

    df

--- backend/controllers/driverController.js
+++ backend/controllers/driverController.js
@@ -14,6 +14,32 @@ export const applyDriver = async (req, res) => {
         message: "Driver application already exists",
       });
     }
+    if (existingDriver.status === "pending") {
+      return res.status(400).json({
+        success: false,
+        message: "Application already under review.",
+      });
+    }
+    if (existingDriver.status === "approved") {
+      return res.status(400).json({
+        success: false,
+        message: "You are already a driver.",
+      });
+    }
+    if (existingDriver.status === "rejected") {
+      return Object.assign(existingDriver, req.body);
+      existingDriver.status = "pending";
+      await existingDriver.save();
+      await User.findByIdAndUpdate(userId, {
+    driverStatus: "pending",
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "Application resubmitted successfully",
+    driver: existingDriver,
+  });
+    }
     const driver = await Driver.create({
       user: userId,
       ...req.body,
