4abae7b750fd4dbf7d411c4397ec57d0cd298e6a
the-junior21 <hamdidajr@gmail.com>
2026-08-11 10:04:29 +0100

    configure the pin stuff 1.2

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ import User from "./models/User.js";
 import savePushToken from "./routes/users/savePushTokenn.js"
 import fetch from "node-fetch"
 import serviceRoutes from "./routes/serviceRoutes.js"
+import pinVerificationRoutes from "./routes/pinVerificationRoutes.js"
 import offersRoutes from "./routes/offersRoutes.js"
 
 
