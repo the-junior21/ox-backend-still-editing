7f3f75980f37c651537d42901432da1cdfb55dc2
the-junior21 <hamdidajr@gmail.com>
2026-08-16 11:00:19 +0100

    add get contact route1.2

--- backend/routes/users/user.routes.js
+++ backend/routes/users/user.routes.js
@@ -84,7 +84,7 @@ router.get("/get/pin/:id", async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
-router.get("/users/:id/emergency-contacts", async (req, res) => {
+router.get("/:id/emergency-contacts", async (req, res) => {
   try {
     const user = await User.findById(req.params.id).select("emergencyContacts");
 
